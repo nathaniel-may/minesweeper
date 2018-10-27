c41f422a8465c1663cc52efa8b194f92c4d73985
Nathaniel May <nathanieldmay@gmail.com>
2018-10-27 19:25:30 -0400

    refactored dimensions and error handling

--- src/main/scala/com/nathanielmay/minesweeper/MineSweeper.scala
+++ src/main/scala/com/nathanielmay/minesweeper/MineSweeper.scala
@@ -1,6 +1,19 @@
 package com.nathanielmay.minesweeper
 
-case class Square(h: Int, v: Int)
+
+case class H(value: Int)
+case class V(value: Int)
+
+case class Dim(h: H, v: V) {
+  val area: Int = h.value * v.value
+
+  def contains(s: Square): Boolean = contains(s.h) && contains(s.v)
+  def contains(x: H):      Boolean = x.value >= 0 && x.value < h.value
+  def contains(x: V):      Boolean = x.value >= 0 && x.value < v.value
+  def isAtLeast(d: Dim):   Boolean = h.value >= d.h.value && v.value >= d.v.value
+}
+
+case class Square(h: H, v: V)
 
 //sum type to disambiguate boolean flags
 private sealed trait Visibility
@@ -26,47 +39,46 @@ case object Lose extends GameResult
 
 object MineSweeper{
 
-  //to be called by companion object only
-  private def apply(hDim: Int, vDim: Int, graph: Map[Square, Tile]): MineSweeper = new MineSweeper(hDim, vDim, graph)
+  //smart constructor called by other sugared apply methods
+  private def apply(dim: Dim, graph: Map[Square, Tile]): Option[MineSweeper] = {
+    if (!dim.isAtLeast(Dim(H(2), V(2)))) None
+    else if (graph.values.count(_.value == Bomb) >= dim.area) None
+    else if (graph.keys.exists(!dim.contains(_))) None
+    else Some(new MineSweeper(dim, graph))
+  }
 
   //standard game creation
-  def apply(hDim: Int, vDim: Int, bombs: Int): MineSweeper = {
-    require(hDim > 1 && vDim > 1, "size of map must be at least 2x2")
-    require(bombs < hDim*vDim, "map must have at least one square without a bomb")
+  def apply(dim: Dim, bombs: Int): Option[MineSweeper] = {
 
     //quadratic function
-    val randBombs = (0 until hDim*vDim).toList.filterNot(
-      List.tabulate(bombs)(x => (scala.math.random()*(hDim*vDim-x)).toInt)
-      .foldLeft((0 until hDim*vDim).toList)((allSquares, bomb) =>
+    val randBombs = (0 until dim.area).toList.filterNot(
+      List.tabulate(bombs)(x => (scala.math.random()*(dim.area-x)).toInt)
+      .foldLeft((0 until dim.area).toList)((allSquares, bomb) =>
         allSquares.splitAt(bomb) match { case (pre, post) => pre ++ post.tail })
       .toSet)
-      .map(rand => Square(rand/hDim.toInt, rand%hDim))
+      .map(rand => Square(H(rand / dim.h.value), V(rand % dim.h.value)))
 
-    MineSweeper(hDim, vDim, randBombs)
+    MineSweeper(dim, randBombs)
   }
 
   //custom game creation for testing
-  def apply(hDim: Int, vDim: Int, bombs: List[Square]): MineSweeper = {
-    require(hDim > 1 && vDim > 1, "size of map must be at least 2x2")
-    require(bombs.size < hDim*vDim, "map must have at least one square without a bomb")
-    require(!bombs.exists(s => s.h >= hDim || s.h < 0 || s.v >= vDim || s.v < 0), "all bombs must be within the bounds of the map")
+  //TODO move this into implicit test helper?
+  def apply(dim: Dim, bombs: List[Square]): Option[MineSweeper] =
+    MineSweeper(dim, buildGraph(dim, bombs))
 
-    MineSweeper(hDim, vDim, buildGraph(hDim, vDim, bombs))
-  }
-
-  private def buildGraph(hDim: Int, vDim: Int, bombs: List[Square]): Map[Square, Tile] = {
+  private def buildGraph(dim: Dim, bombs: List[Square]): Map[Square, Tile] = {
     def neighbors(square: Square): List[Square] =
       (for {
         hDiff <- -1 to 1
         vDiff <- -1 to 1
         if !(hDiff == 0 && vDiff == 0)
-        neighbor = Square(square.h + hDiff, square.v + vDiff)
-        if withinBounds(neighbor, hDim, vDim)
+        neighbor = Square(H(square.h.value + hDiff), V(square.v.value + vDiff))
+        if dim.contains(neighbor)
       } yield neighbor).toList
 
     val bombMap = bombs.map(_ -> Bomb).toMap
 
-    (for{h <- 0 until hDim; v <- 0 until vDim} yield Square(h, v))
+    (for{h <- 0 until dim.h.value; v <- 0 until dim.v.value} yield Square(H(h), V(v)))
       .map(square => square -> bombMap.getOrElse(square, NearBombs(-1)))
       .toMap
       .foldLeft(Map[Square, Tile]())((m, kv) => kv._2 match {
@@ -74,20 +86,13 @@ object MineSweeper{
         case NearBombs(_) => m.updated(kv._1, Tile(Hidden, NearBombs(neighbors(kv._1).count(bombMap.getOrElse(_, "") == Bomb)), neighbors(kv._1)))
       })
   }
-
-  private def withinBounds(square: Square, hDim: Int, vDim: Int): Boolean =
-    square.h < hDim && square.h >= 0 &&
-    square.v < vDim && square.v >= 0
 }
 
-class MineSweeper private (hDim: Int, vDim: Int, graph: Map[Square, Tile]) {
+//cannot make case class because of overloaded apply smart constructor
+class MineSweeper private (dim: Dim, graph: Map[Square, Tile]) {
 
   //called when UI receives a click
   def reveal(square: Square): MineSweeper = {
-    require(withinBounds(square), "choose a square within the bounds of the board")
-    require(Hidden == graph(square).visibility, "this tile has already been revealed")
-    require(state.isEmpty, "cannot continue a completed game")
-
     //recursive function for when a zero is clicked
     def floodReveal(toReveal: Square, midFloodGraph: Map[Square, Tile]): Map[Square, Tile] = {
       graph(toReveal).value match {
@@ -99,7 +104,10 @@ class MineSweeper private (hDim: Int, vDim: Int, graph: Map[Square, Tile]) {
       }
     }
 
-    MineSweeper(hDim, vDim, floodReveal(square, graph))
+    if (!dim.contains(square)) this
+    else if (Hidden != graph(square).visibility) this
+    else if (state.isDefined) this
+    else MineSweeper(dim, floodReveal(square, graph)).get
   }
 
   //exposing only public types to UI
@@ -122,13 +130,10 @@ class MineSweeper private (hDim: Int, vDim: Int, graph: Map[Square, Tile]) {
     stateCheck(graph.values.toList, 0, 0)
   }
 
-  private def withinBounds(square: Square): Boolean =
-    MineSweeper.withinBounds(square, hDim, vDim)
-
   //for printing purposes
   override def toString: String = {
-    (0 until vDim).toList
-      .map(v => (0 until hDim).toList.map(h => Square(h, v)))
+    (0 until dim.v.value).toList
+      .map(v => (0 until dim.h.value).toList.map(h => Square(H(h), V(v))))
       .map(row => row.map(revealedTiles.getOrElse(_, " ").toString).mkString("|","|","|"))
       .mkString("\n")
   }
@@ -142,7 +147,7 @@ object Play{
          Square(3, 3),
          Square(1, 3),
          Square(0, 3))
-      .foldLeft(MineSweeper(4, 4, List(Square(2,3), Square(0,2))))((game, square) =>
+      .foldLeft(MineSweeper(Dim(4,4), List(Square(2,3), Square(0,2))).get)((game, square) =>
         takeTurn(game, square)
       ).state.getOrElse("Keep Playing"))
   }
@@ -153,6 +158,9 @@ object Play{
     println()
     game.reveal(square)
   }
+
+  implicit def hAble(i: Int): H = H(i)
+  implicit def vAble(i: Int): V = V(i)
 
 }
 
