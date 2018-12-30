615f064a1c4dea380fd1da373762521e4657e6ce
Nathaniel May <nathanieldmay@gmail.com>
2018-12-30 15:30:38 -0500

    lazy optimization for full range property

--- src/test/scala/com/nathanielmay/minesweeper/GameProperties.scala
+++ src/test/scala/com/nathanielmay/minesweeper/GameProperties.scala
@@ -1,16 +1,17 @@
 package com.nathanielmay.minesweeper
 
-//scalacheck
+// scalacheck
 import org.scalacheck.{Properties, Prop, Gen}, Prop.{BooleanOperators, forAll}, Gen.choose
 
-//testing
-import testingUtil.Arbitrarily.{aDim, getDimGen, aRun}
-import testingUtil.Util.{Run, TestableGame}
+// scala
+import scalaz.Scalaz.unfold
 
-//project
+// project
+import testingUtil.Arbitrarily.{aDim, getDimGen, aRun}
+import testingUtil.Util.Run
 import MineSweeper.randBombs
 
-object GameProperties extends Properties("MineSweeper game"){
+object GameProperties extends Properties("MineSweeper game") {
 
   property("can't have negative bombs") = forAll {
     (d: Dim, i: Int) =>
@@ -36,15 +37,21 @@ object GameProperties extends Properties("MineSweeper game"){
     } yield bombs.forall(d.contains)).eval(new java.util.Random(seed))
   }
 
-  //TODO not a pure property
-  property("randBombs generates bombs in the full range of tiles") = forAll(getDimGen(7)) {
-    (dim: Dim) => {
-      val bombs: Int = scala.math.ceil(dim.area / 5.0).toInt
+  property("randBombs generates bombs in the full range of tiles") = forAll(choose(Long.MinValue, Long.MaxValue), getDimGen(7)) {
+    (seed: Long, dim: Dim) => {
+      def noLessThan(d: Double, min: Int): Int = if (d.toInt < min) min else d.toInt
+      val infiniteSamples: Stream[Set[Square]] = unfold(new scala.util.Random(seed)) { r =>
+        Some((randBombs(dim, noLessThan(dim.area / 2.0, 1)).eval(r).toSet, r)) }
 
-      Stream.fill(dim.area * dim.area / bombs)(randBombs(dim, bombs))
-        .foldLeft[Set[Square]](Set()) { (l, rand) =>
-          rand.eval(new scala.util.Random(System.nanoTime())).toSet ++ l }
-        .size == dim.area
+      def go(set: Set[Square], samples: Stream[Set[Square]], attempts: Int): Boolean =
+        if (set.size == dim.area) true
+        else if (attempts <= 0) false
+        else samples match {
+          case sample #:: moreSamples => go(set ++ sample, moreSamples, attempts-1)
+          case _                      => false
+        }
+
+      go(Set(), infiniteSamples, 100)
     }
   }
 
